a5e12baddf7ed54350ad1808aa43ebe7e2fb0c05
ishantbhurani <67356291+ishantbhurani@users.noreply.github.com>
2024-05-16 00:59:21 +0530

    add intro section

---
+++ components/intro.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import Image from "next/image";
+import { motion } from "framer-motion";
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import avatarImg from "@/public/avatarImg.jpg";
+import { useSectionInView } from "@/lib/hooks";
+
+export default function Intro() {
+  const { ref } = useSectionInView("Home", 0.5);
+
+  return (
+    <section
+      id="home"
+      ref={ref}
+      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
+    >
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ type: "tween", duration: 0.2 }}
+          >
+            <Image
+              src={avatarImg}
+              alt="Ishant Bhurani"
+              quality="95"
+              priority={true}
+              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+            />
+          </motion.div>
+          <motion.span
+            className="text-4xl absolute bottom-0 right-0"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            👋
+          </motion.span>
+        </div>
+      </div>
+
+      <motion.h1
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hello, I&apos;m Ishant Bhurani.</span>{" "}
+        I&apos;m a <span className="font-bold">Flutter developer</span>. I enjoy
+        building <span className="italic">apps</span>, and solving complex
+        problems.
+      </motion.h1>
+
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-4 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.1 }}
+      >
+        <a
+          href="/Ishant_Bhurani_Resume.pdf"
+          download
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+        >
+          Download CV{" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+        <div className="flex gap-2 items-center justify-center">
+          <a
+            href="https://www.linkedin.com/in/ishant-bhurani"
+            target="_blank"
+            className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition borderBlack dark:bg-white/10 dark:text-white/60"
+          >
+            <BsLinkedin />
+          </a>
+
+          <a
+            href="https://github.com/ishantbhurani"
+            target="_blank"
+            className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full text-[1.35rem] outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition borderBlack dark:bg-white/10 dark:text-white/60"
+          >
+            <BsGithub />
+          </a>
+        </div>
+      </motion.div>
+    </section>
+  );
+}
