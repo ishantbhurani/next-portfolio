637c5502a841a33523c7adce95ca8965561aef84
ishantbhurani <67356291+ishantbhurani@users.noreply.github.com>
2024-05-16 01:35:26 +0530

    reorder skills and projects sections

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
       <Intro />
       <SectionDivider />
       <About />
-      <Projects />
       <Skills />
+      <Projects />
     </main>
   );
 }
--- lib/data.ts
+++ lib/data.ts
@@ -12,12 +12,12 @@ export const links = [
     hash: "#about",
   },
   {
-    name: "Projects",
-    hash: "#projects",
-  },
-  {
     name: "Skills",
     hash: "#skills",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
   },
 ] as const;
 
