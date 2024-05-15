9e51901c3293eb6d284bfe7cf67f10fd5cda7d72
ishantbhurani <67356291+ishantbhurani@users.noreply.github.com>
2024-05-16 00:29:13 +0530

    add static data

---
+++ lib/data.ts
@@ -0,0 +1,81 @@
+import flutterTodoImg from "@/public/flutterTodoImg.jpg";
+import moneytrackerImg from "@/public/moneytrackerImg.png";
+import deluxxeImg from "@/public/deluxxeImg.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "Flutter Todo App",
+    description:
+      "An elegant Todo App built with Flutter. It supports dark mode, provider, and data persistence with Isar.",
+    tags: ["Flutter", "Provider", "Isar", "Shared Preferences"],
+    url: "https://github.com/ishantbhurani/flutter-todo-app",
+    imageUrl: flutterTodoImg,
+  },
+  {
+    title: "Money Tracker",
+    description:
+      "A fully responsive MERN stack app to track your daily expenses. It supports JWT based email & password authentication.",
+    tags: [
+      "React",
+      "Redux",
+      "Node.js",
+      "Express",
+      "MongoDB",
+      "Tailwind",
+      "TypeScript",
+    ],
+    url: "https://money-tracker-client.netlify.app",
+    imageUrl: moneytrackerImg,
+  },
+  {
+    title: "DeLuxxe",
+    description:
+      "A fully responsive e-commerce app. It support Google authentication. It has a product carousel, products list, and slide-in cart modal.",
+    tags: ["React", "TypeScript", "Tailwind", "Firebase"],
+    url: "https://deluxxe.netlify.app",
+    imageUrl: deluxxeImg,
+  },
+] as const;
+
+export const skillsData = [
+  "Dart",
+  "Python",
+  "C/C++",
+  "JavaScript",
+  "TypeScript",
+  "HTML/CSS",
+  "Flutter",
+  "Hive",
+  "Isar",
+  "Provider",
+  "React",
+  "Tailwind CSS",
+  "Firebase",
+  "Internet of Things (IoT)",
+  "Arduino",
+  "Git",
+  "GitHub",
+  "VS Code",
+  "Android Studio",
+  "Sublime",
+  "Postman",
+] as const;
